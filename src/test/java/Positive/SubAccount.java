a6f1180b5cc2ef93d150eda76868cdf2ad8705b4
Sachintha <98406068+Sachintha-Samarathunga@users.noreply.github.com>
2025-04-07 23:45:03 +0530

    Add xpaths for sub account

--- src/test/java/Positive/SubAccount.java
+++ src/test/java/Positive/SubAccount.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import utils.BaseTest;
 import utils.ExtentReportManager;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class SubAccount extends BaseTest {
@@ -21,8 +22,34 @@ public class SubAccount extends BaseTest {
     }
 
     @Test(priority = 1)
-    public void createSubAccount(){
+    public void createSubAccount() throws InterruptedException, AWTException {
+        ExtentReportManager.startTest("Test Cases for Sub Account Creation", "<b>Create a Sub Account</b>");
 
+        webSteps.click("settings_Tab");
+        webSteps.click("subAccount_Tab");
+        webSteps.click("sct_AddBtn");
+
+        webSteps.type("Sachinthax", "sc_companyName");
+        webSteps.type("REG124780cbc", "sc_regCode");
+        webSteps.type("971501234567","sc_companyNum");
+        webSteps.type("TAX1247525", "sc_taxNum");
+        webSteps.type("sachinthax20478@gmail.com", "sc_email");
+        webSteps.type("971501234567","sc_phoneNum");
+        webSteps.type("971501234567","sc_whatsappNum");
+        webSteps.select("sc_country", 2, 1);
+        webSteps.select("sc_state", 3, 1);
+
+        webSteps.scrollToElement("scf_AddBtn");
+
+        webSteps.type("Sachintha", "scp_firstName");
+        webSteps.type("Samarathunga", "scp_lastName");
+        webSteps.type("12345@Aa", "scp_password");
+        webSteps.type("12345@Aa", "scp_confPassword");
+        webSteps.type("sachintha20478@gmail.com", "scp_email");
+        webSteps.type("971501234567","scp_phoneNum");
+        webSteps.type("971501234567","scp_whatsappNum");
+        webSteps.select("scp_country", 2, 1);
+        webSteps.select("scp_state", 3, 1);
     }
 
 }
